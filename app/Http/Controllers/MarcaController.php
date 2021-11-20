4b48b8a09a70f74b54ed3066f4ebec54251d68b0
Douglas <vulcano.pgm@gmail.com>
2021-11-20 10:38:49 -0300

    upload de imagens

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -7,24 +7,21 @@ use Illuminate\Http\Request;
 
 class MarcaController extends Controller
 {
+
+    public function __construct(Marca $marca) {
+        $this->marca = $marca;
+    }
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    {   
+        //$marcas = Marca::all();
+        $marcas = $this->marca->all();
+        return response()->json($marcas, 200);
     }
 
     /**
@@ -34,52 +31,110 @@ class MarcaController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {
-        //
+    {   
+
+        /** Tratamento e validação dos parametros */
+        $request->validate($this->marca->rules(), $this->marca->feedback());
+
+        /** dd($request->get('nome')); */
+        /** dd($request->file('imagem')); */
+
+        $image = $request->file('imagem');
+
+        /**
+         * O método store recebe dois parametros para armazenar a imagem
+         * store(<PATH>, <DISCO>)
+         * O DISCO é configurado em config/filesystems.php 
+         * Existem 3 tipos: local, public e cloud (AWS S3)
+         * Por padrão o disco é setado para local
+         */
+
+        $image->store('imagens', 'public');
+
+        dd('Upload de arquivos');
+
+        //$marca = $this->marca->create($request->all());
+        return response()->json($marca, 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function show(Marca $marca)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Marca  $marca
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Marca $marca)
-    {
-        //
+    public function show($id)
+    {   
+        $marca = $this->marca->find($id);
+        if($marca === null) {
+            return response()->json([
+                'error' => "This register dont't exists."
+            ], 404);
+        }
+        return response()->json($marca, 200);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Marca $marca)
-    {
-        //
+    public function update(Request $request, $id)
+    {   
+        // $marcas = Marca::find($marca->id);
+        $marca = $this->marca->find($id);
+        
+        if ($marca === null) {
+            return response([
+                'error' => "This register dont't exists."
+            ], 404);
+        }
+
+        if ($request->method() == 'PATCH'){
+
+            $dinamicRules = array();
+
+
+            foreach($marca->rules() as $input => $rule) {
+
+                /** Coleta apenas as regras parciais da requisição PATCH */
+                if(array_key_exists($input, $request->all())) {
+                    $dinamicRules[$input] = $rule;
+                }
+            }
+
+            $request->validate($dinamicRules);
+
+        } else {
+
+            $request->validate($this->marca->rules(), $this->marca->feedback());
+        }
+
+        $marca->update($request->all());
+        return response()->json($marca, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Marca $marca)
-    {
-        //
+    public function destroy($id)
+    {   
+        $marca = $this->marca->find($id);
+        if ($marca === null) {
+            return response([
+                "error" => "This record can't be deleted."
+            ], 404);
+        }
+
+        $marca->delete();
+        return [
+            'msg' => 'A marca foi deletada com sucesso!'
+        ];
     }
 }
+ 
--- app/Models/Marca.php
+++ app/Models/Marca.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Marca extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nome', 'imagem'
+    ];
+
+    public function rules() {
+        return [
+            'nome' => 'required|unique:marcas,nome,'.$this->id,
+            'imagem' => 'required'
+        ];
+
+        /*
+            1) tabela
+            2) nome da coluna que será pesquisada na tabela
+            3) id do registro que será desconsiderado na pesquisa
+        */
+    }
+
+    public function feedback() {
+        return [
+            'required' => 'O campo :attribute é obrigatório',
+            'nome.unique' => 'O nome da marca já existe'
+        ];
+    }
 }
